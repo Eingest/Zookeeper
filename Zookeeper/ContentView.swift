37d2484293bd7393353db76f15e9e2cfcd93fe7d
Eingest <ak0805@live.de>
2022-04-20 10:24:32 +0200

    Initial Commit

---
+++ Zookeeper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Zookeeper
+//
+//  Created by Andreas Kiesel on 20.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Zookeeper/ZookeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeeperApp.swift
+//  Zookeeper
+//
+//  Created by Andreas Kiesel on 20.04.22.
+//
+
+import SwiftUI
+
+@main
+struct ZookeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
